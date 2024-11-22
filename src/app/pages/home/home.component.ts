7d54621ee8ee5f5e0780907a6a82e2d2342b57ea
alice farias <alice.farias204@gmail.com>
2024-11-21 23:23:47 -0300

    fiz a faixa rotativa, fiz o detalhe do produto lançamento onça preta, falta terminar parte de lançamento com prodtos

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../../footer/footer.component';
 import { AnimacaoComponent } from "../../animacao/animacao.component";
+import { LancamentoComponent } from "../../lancamento/lancamento.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FooterComponent, AnimacaoComponent],
+  imports: [FooterComponent, AnimacaoComponent, LancamentoComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
